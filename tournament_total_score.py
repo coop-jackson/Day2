fc8cef1d54ffeff77e351c2ac4c4550893ce14c6
Cooper Jackson <scoop.jackson1234@gmail.com>
2022-12-02 11:42:49 -0600

    Part-2

--- tournament_total_score.py
+++ tournament_total_score.py
@@ -4,16 +4,19 @@
 #Second column ? Assumed what should be played  X         Y          Z Respectively
 #Winner determined by highest score. 
 #Total Score - sum of scores for each round.
-#PTS: Rock - 1, Paper - 2, Scissors - 3; Outcome: lost - 0, draw: - 3, won - 6
+#PTS: Rock - 1, Paper - 2, Scissors - 3; Outcome: lost - 0, draw: - 3, win - 6
 #unsure if being tricked.
 #Find score if everything goes like strategy guide.
-A, X = 1, 1
-B, Y = 2, 2
-C, Z = 3, 3
+#Part 2 - X - lose, Y - draw, Z - win
 
-draw, win = 3, 6
+A, B, C = 1, 2, 3
+X, Y, Z = 'lose', 'draw', 'win'
 rounds = []
 i = 1
+template = [1, 2, 3]
+lose = [3, 1, 2]
+win = [2, 3, 1]
+
 with open('Strategy_guide.txt') as file:
     for line in file.readlines():
         code = line.replace('\n', '').split(' ')
@@ -21,12 +24,20 @@ with open('Strategy_guide.txt') as file:
         enemy_elf = eval(code[0])
         me_elf = eval(code[1])
 
-        if enemy_elf == me_elf:
-            rounds.append(me_elf + draw)
-        elif (enemy_elf - me_elf == 1 or enemy_elf - me_elf == -2):            
-            rounds.append(me_elf)
-        elif (enemy_elf - me_elf == -1 or enemy_elf - me_elf == 2):
-            rounds.append(me_elf + win)        
+        if me_elf == 'draw':
+            rounds.append(enemy_elf + 3)
+        elif me_elf == 'lose':            
+            for i in range(len(template)):
+                if enemy_elf == template[i]:
+                    rounds.append(lose[i])
+                    break
+            
+        elif me_elf == 'win':
+            for i in range(len(template)):
+                if enemy_elf == template[i]:
+                    rounds.append(win[i] + 6)
+                    break
+
 file.close()
 
 score = sum(rounds)
